ca6ee0b3d9dec4e0f5906f8b3a88817ef67eb25a
Yourartbest <yourartbest@gmail.com>
2026-03-04 03:17:30 -0500

    navbar changes for none list dots

--- apps/web/src/navbar/navbar.tsx
+++ apps/web/src/navbar/navbar.tsx
@@ -1,12 +1,22 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 export default function Navbar() {
+    const [isOpen, setIsOpen] = useState(true);
+
     return (
         <nav className="sidebar">
-            <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/profile">Profile</Link></li>
-            </ul>
+            <div>
+                <button className="md:hidden text-2xl" onClick={() => setIsOpen(!isOpen)}>
+                {isOpen ? "" : "☰"}
+                </button>
+            </div>
+            <div className={`flex-col md:flex md:flex-row gap-4 ${isOpen ? "translate-x-0" : "-translate-x-full"}`}>
+                <ul className="list-none center">
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/profile">Profile</Link></li>
+                </ul>
+            </div>
         </nav>
     );
 }
