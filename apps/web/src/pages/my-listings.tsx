396c9485a705b4fa0b3bd09fd81ecd8b287b312d
Alexey Mishin <alexeymishin023@gmail.com>
2026-03-28 11:51:54 -0400

    Fix: reverted the chnages to my-lisitng.tsx

--- apps/web/src/pages/my-listings.tsx
+++ apps/web/src/pages/my-listings.tsx
@@ -1,3 +1,10 @@
+import React from "react";
+import type { SessionUser } from "../features/types";
+
+type OutletContext = {
+    user: SessionUser | null;
+};
+
 const MyListings = () => {
     return (
         <div>
